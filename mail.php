25bb0393b06ca18b1220235ac2bf75f9ee29045a
richarddushime <mudaherarich@gmail.com>
2024-02-21 01:33:24 +0300

    final

--- mail.php
+++ mail.php
@@ -5,34 +5,31 @@ require 'phpmailer/class.phpmailer.php';
 $mail = new PHPMailer;
 $mail->isSMTP();
 
-$mail->Host='smtp.gmail.com';
-$mail->Port=587;
-$mail->SMTPAuth=true;
-$mail->SMTPSecure='tls';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
 
-      $name=$_POST['name'];
-      $email=$_POST['email'];
-      $phone=$_POST['phone']
-      $message=$_POST['message'];
+$name = $_POST['fname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
-//$address = "example@bluebird.net";
-$mail->Username= "infos@tirinyibeekeepers.org";
-$mail->Password='@tirinyi.1';
+$mail->Username = "infos@tirinyibeekeepers.org";
+$mail->Password = '@tirinyi.1';
 
-$mail->setFrom($email," Tirinyi Website");
+$mail->setFrom($email, "Tirinyi Website");
 $mail->addAddress('tirinyi@gmail.com');
 $mail->addReplyTo($email);
 
 $mail->isHTML(true);
-$mail->Subject = 'You\'ve been contacted by ' . $name .'.';
-$mail->Body = " <h2 align=center> Name : ". $name ."</h2><br> <h2 align=center> Phone Number : ". $phone ."</h2> <br>
-            <h2 align=center> Email : ". $email ."</h2><br><p align=center>" .$message. "</p><br>" ;
-$mail->Content =" ";
+$mail->Subject = 'You\'ve been contacted by ' . $name . '.';
+$mail->Body = "<h2 align=center>Name: " . $name . "</h2><br><h2 align=center>Phone Number: " . $phone . "</h2><br>
+                <h2 align=center>Email: " . $email . "</h2><br><p align=center>" . $message . "</p><br>";
 
-if (!$mail->send()){
-      header("Location: index.html");
-      echo $mail->ErrorInfo;
-}else{
-      header("Location:contact-us.html");    
-} 
+if (!$mail->send()) {
+    echo "Message could not be sent. Mailer Error: " . $mail->ErrorInfo;
+} else {
+    header("Location: contact-us.html");
+}
 ?>
